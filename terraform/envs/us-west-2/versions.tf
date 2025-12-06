f6c5dec263d0d9b7a6e9d58caa40717edbf4eca9
Rajesh Doddi <35097219+doddirajeshreddy@users.noreply.github.com>
2025-12-06 02:16:21 -0600

    Update versions.tf

--- terraform/envs/us-west-2/versions.tf
+++ terraform/envs/us-west-2/versions.tf
@@ -1,4 +1,12 @@
 terraform {
+  backend "s3" {
+    bucket         = "skylo-tfstate-prod"
+    key            = "regional-hub/us-west-2/terraform.tfstate"
+    region         = "us-west-2"
+    encrypt        = true
+    dynamodb_table = "skylo-tfstate-lock"
+  }
+
   required_version = ">= 1.5.0"
 
   required_providers {
